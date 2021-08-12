fe195ec8735235b58a1a649339500e7f4e90808a
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-08-12 18:52:01 +0200

    First attempt

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,52 @@
+<nav>
+    <div class="
+    {{-- bg-yellow-100 bg-opacity-75  --}}
+    mx-auto px-2 sm:px-6 lg:px-8 mb-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex-shrink-0 flex items-center">
+            <img class="h-20 w-auto" src="https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/122033367_108942074328102_6833554159246771634_n.jpg?_nc_cat=110&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=INfInSPkPV0AX-IcLSc&_nc_ht=scontent.fbru1-1.fna&oh=91deb2cb3b4d134c32b27dee70ce5ebc&oe=613B3D01" alt="Workflow">
+          </div>
+          <div class="sm:ml-6 flex items-center">
+            <div class="space-x-4 ">
+              <!-- Current: "bg-gray-900 ", Default: "     hover:" -->
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium">SCOBY</a>
+  
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium">ABOUT US</a>
+  
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium">EVENTS</a>
+  
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium">SHOP</a>
+            </div>
+          </div>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div>
+                0
+            </div>
+  
+            <div class="ml-3 relative">
+                <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    {{-- <!-- Mobile menu, show/hide based on menu state. -->
+    <div class="sm:hidden" id="mobile-menu">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <!-- Current: "bg-gray-900 ", Default: "     hover:" -->
+        <a href="#" class="bg-gray-900  block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+  
+        <a href="#" class="     hover: block px-3 py-2 rounded-md text-base font-medium">Team</a>
+  
+        <a href="#" class="     hover: block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+  
+        <a href="#" class="     hover: block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+      </div>
+    </div> --}}
+</nav>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <p class="bg-yellow-100 bg-opacity-50 py-40 px-4">This is my body content.</p>
+@endsection
